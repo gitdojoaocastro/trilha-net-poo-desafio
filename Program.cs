78804fbed451bd35c1caaabefcbe9b8a03dfc9c6
gitdojoaocastro <joaovictor0397@gmail.com>
2023-12-07 23:03:26 -0300

    Correções realizadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("62982263485", "Nokia 2630", "8945415321", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("62987442147", "Iphone XR", "9854454525", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Duolingo");
